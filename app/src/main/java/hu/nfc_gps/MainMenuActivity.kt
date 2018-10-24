80cfb8f51d4ab86163c914ce8ce9bceb148fb923
Toppantó Tamás <toppanto96@gmail.com>
2018-10-24 21:09:41 +0200

    Add: tab layout and viewpager in main menu Add: MapsFragment Add: CardsFragment

--- app/src/main/java/hu/nfc_gps/MainMenuActivity.kt
+++ app/src/main/java/hu/nfc_gps/MainMenuActivity.kt
@@ -1,12 +1,17 @@
 package hu.nfc_gps
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main_menu.*
 
 class MainMenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+        tabs_view_pager.adapter = TabsPagerAdapter(supportFragmentManager)
+
+        tabs.setupWithViewPager(tabs_view_pager)
     }
 }
---
+++ app/src/main/java/hu/nfc_gps/TabsPagerAdapter.kt
@@ -0,0 +1,33 @@
+package hu.nfc_gps
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class TabsPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
+
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                CardsFragment()
+            }
+            else -> {
+                return MapsFragment()
+            }
+        }
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when (position) {
+            0 -> "Kártyák"
+            else -> {
+                return "Térkép"
+            }
+        }
+    }
+}
